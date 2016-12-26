b0bc0b5c98518700064b0a610275a49584f02f2d
Astonep <imland@outlook.com>
2016-12-27 01:31:59 +0800

    Update database.php

--- app/database.php
+++ app/database.php
@@ -15,11 +15,7 @@ return [
     // 服务器地址
     'hostname'       => '127.0.0.1',
     // 数据库名
-<<<<<<< HEAD
-    'database'       => 'neweb',
-=======
     'database'       => 'tp-admin',
->>>>>>> origin/master
     // 用户名
     'username'       => 'root',
     // 密码
