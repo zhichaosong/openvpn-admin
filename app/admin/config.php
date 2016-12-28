79843d3a5e7b5bda61096e65b11bd8ba2184614f
Astonep <imland@outlook.com>
2016-12-28 18:52:27 +0800

    验证码页面

--- app/admin/config.php
+++ app/admin/config.php
@@ -64,6 +64,26 @@ return [
         '__LIB__'    => STATIC_PATH . 'admin/lib'
     ],
 
+    //验证码
+
+    'captcha'  => [
+        // 验证码字符集合
+        'codeSet'  => '2345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY', 
+        // 验证码字体大小(px)
+        'fontSize' => 25, 
+        // 是否画混淆曲线
+        'useCurve' => true, 
+         // 验证码图片高度
+        'imageH'   => 30,
+        // 验证码图片宽度
+        'imageW'   => 100, 
+        // 验证码位数
+        'length'   => 5, 
+        // 验证成功后是否重置        
+        'reset'    => true
+    ],
+
     //伪静态
     'url_html_suffix' => false,
+    'user_auth_key'     => 'authId',
 ];
